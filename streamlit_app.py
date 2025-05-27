178416a1afded170b8bf2690e0c65af3cf150113
azharfauzan4 <fauzanzarr@gmail.com>
2025-05-27 07:55:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! .")
 st.image("IMG_20250501_103018.jpg", width=200)
+
+st.title("XF")
+st.header("XF")
+angka = st.number_input("13:", value=0, step=1)
+
+if (angka % 2) ==0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
